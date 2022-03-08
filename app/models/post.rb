ff38a07c1531165ebfd77e71bfe078093109d07a
Louis <louis.harel@kellogg.northwestern.edu>
2022-03-08 15:31:01 +0000

    Coding along

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :uploaded_image # can name this whatever you want
+  # allows us to attach a file to a post and reference it wherever it lives 
 end
